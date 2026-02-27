099471dcb94a47acd449b67f00cde3f5fd0b70d1
benoit souillat <dev.souillat@gmail.com>
2026-02-27 14:41:08 +0100

    modify migration to load .sql before modification

--- migrations/Version20260130153725.php
+++ migrations/Version20260130153725.php
@@ -21,6 +21,14 @@ final class Version20260130153725 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE media ALTER path DROP NOT NULL');
+
+        $sqlDirectory = __DIR__ . '/sql/';
+
+        // Lecture et exécution des fichiers SQL
+        // Tu peux rajouter une vérification file_exists() si tu veux être plus prudent
+        $this->addSql(file_get_contents($sqlDirectory . 'album.sql'));
+        $this->addSql(file_get_contents($sqlDirectory . 'user.sql'));
+        $this->addSql(file_get_contents($sqlDirectory . 'media.sql'));
     }
 
     public function down(Schema $schema): void
---
+++ migrations/sql/album.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.album (id, name) VALUES (1, 'Album 1');
+INSERT INTO public.album (id, name) VALUES (2, 'Album 2');
+INSERT INTO public.album (id, name) VALUES (3, 'Album 3');
+INSERT INTO public.album (id, name) VALUES (4, 'Album 4');
+INSERT INTO public.album (id, name) VALUES (5, 'Album 5');
