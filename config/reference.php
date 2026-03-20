9b7eb42f3821c8ab81a2d1caa362ceb941ca708c
benoit souillat <dev.souillat@gmail.com>
2026-03-20 14:46:28 +0100

    Install Symfony Casts Bundle Reset-password

--- config/reference.php
+++ config/reference.php
@@ -1614,6 +1614,12 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     skip_translation_on_load?: bool|Param, // Default: false
  *     metadata_cache_pool?: scalar|Param|null, // Default: null
  * }
+ * @psalm-type SymfonycastsResetPasswordConfig = array{
+ *     request_password_repository: scalar|Param|null, // A class that implements ResetPasswordRequestRepositoryInterface - usually your ResetPasswordRequestRepository.
+ *     lifetime?: int|Param, // The length of time in seconds that a password reset request is valid for after it is created. // Default: 3600
+ *     throttle_limit?: int|Param, // Another password reset cannot be made faster than this throttle time in seconds. // Default: 3600
+ *     enable_garbage_collection?: bool|Param, // Enable/Disable automatic garbage collection. // Default: true
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1627,6 +1633,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     monolog?: MonologConfig,
  *     vich_uploader?: VichUploaderConfig,
  *     stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
+ *     symfonycasts_reset_password?: SymfonycastsResetPasswordConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
  *         parameters?: ParametersConfig,
@@ -1643,6 +1650,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         maker?: MakerConfig,
  *         vich_uploader?: VichUploaderConfig,
  *         stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
+ *         symfonycasts_reset_password?: SymfonycastsResetPasswordConfig,
  *     },
  *     "when@prod"?: array{
  *         imports?: ImportsConfig,
@@ -1657,6 +1665,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         monolog?: MonologConfig,
  *         vich_uploader?: VichUploaderConfig,
  *         stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
+ *         symfonycasts_reset_password?: SymfonycastsResetPasswordConfig,
  *     },
  *     "when@test"?: array{
  *         imports?: ImportsConfig,
@@ -1672,6 +1681,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         monolog?: MonologConfig,
  *         vich_uploader?: VichUploaderConfig,
  *         stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
+ *         symfonycasts_reset_password?: SymfonycastsResetPasswordConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
--- src/Service/UserMailerService.php
+++ src/Service/UserMailerService.php
@@ -5,28 +5,38 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
 
 class UserMailerService
 {
 
     public function __construct(
-        private readonly MailerInterface $mailer
+        private readonly MailerInterface $mailer,
+        private readonly ResetPasswordHelperInterface $resetPasswordHelper,
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly ParameterBagInterface $params
     )
     {
     }
 
-    public function sendWelcomeEmail(User $guest, ParameterBagInterface $params): void
+    public function sendWelcomeEmail(User $guest): void
     {
+        $resetToken = $this->resetPasswordHelper->generateResetToken($guest);
+        $url = $this->urlGenerator->generate('reset_password', ['token' => $resetToken->getToken()], UrlGeneratorInterface::ABSOLUTE_URL);
+
         $email = (new TemplatedEmail())
-            ->from($params->get('site_email'))
+            ->from($this->params->get('site_email'))
             ->to($guest->getEmail())
-            ->subject("Bienvenue sur " . $params->get('site_name'))
+            ->subject("Bienvenue sur " . $this->params->get('site_name'))
             ->htmlTemplate('emails/user-created.html.twig')
             ->context([
                 "user" => $guest,
+                "setup_password_url" => $url,
             ]);
         $this->mailer->send($email);
 
